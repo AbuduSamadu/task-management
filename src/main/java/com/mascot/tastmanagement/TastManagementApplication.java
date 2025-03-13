e4b4350dfcd5b3390a89d5679d7d500b59b36764
abudusamad <abudusamed@gmail.com>
2025-03-13 00:05:04 +0000

    sadsd

--- src/main/java/com/mascot/tastmanagement/TastManagementApplication.java
+++ src/main/java/com/mascot/tastmanagement/TastManagementApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TastManagementApplication {
 
+    
     public static void main(String[] args) {
         SpringApplication.run(TastManagementApplication.class, args);
     }
